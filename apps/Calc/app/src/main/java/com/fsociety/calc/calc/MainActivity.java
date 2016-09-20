71df92d46432d47184fb4c5c763dadd9428bd02c
Geilson Silva <geilsonpereira@outlook.com.br>
2016-09-20 19:42:04 -0300

    #1 - Modificação no Layout e Interação

--- apps/Calc/app/src/main/java/com/fsociety/calc/calc/MainActivity.java
+++ apps/Calc/app/src/main/java/com/fsociety/calc/calc/MainActivity.java
@@ -3,14 +3,14 @@ package com.fsociety.calc.calc;
 import android.support.v7.app.AppCompatActivity;
 import android.os.Bundle;
 import android.view.View;
-import android.widget.*; //Importa todos os componentes do pacote widget *
+import android.widget.*; //Importa todos os componentes widget *
 
 public class MainActivity extends AppCompatActivity {
 
-    private EditText formNota01;
-    private EditText formNota02;
-    private Button btnCalcular;
-    private TextView textResultado;
+    private EditText formNota01; // Declarada Variável formNota01
+    private EditText formNota02; // Declarada Variável formNota01
+    private Button btnCalcular; // Declarada Variável btnCalcular
+    private TextView textResultado; // Declarada Variável textResultado
     private double nota01;
     private double nota02;
 
@@ -19,22 +19,26 @@ public class MainActivity extends AppCompatActivity {
         super.onCreate(savedInstanceState);
         setContentView(R.layout.activity_main);
 
-        formNota01 = (EditText)findViewById(R.id.formNota01);
-        formNota02 = (EditText)findViewById(R.id.formNota02);
-        btnCalcular = (Button)findViewById(R.id.btnCalcular);
-        textResultado = (TextView)findViewById(R.id.textResultado);
-        btnCalcular.setOnClickListener(new View.OnClickListener() {
+        formNota01 = (EditText)findViewById(R.id.formNota01); //Formulário NOTA 1
+        formNota02 = (EditText)findViewById(R.id.formNota02); //Formulário NOTA 2
+        btnCalcular = (Button)findViewById(R.id.btnCalcular); //Botão Resultado
+        textResultado = (TextView)findViewById(R.id.textResultado); //Resultado Final
+        btnCalcular.setOnClickListener(new View.OnClickListener() { // Ação depois do Click
             @Override
             public void onClick(View v) {
                 nota01 = Double.parseDouble(formNota01.getText().toString());
                 nota02 = Double.parseDouble(formNota02.getText().toString());
 
                 Double media = calcularMedia(nota01, nota02);
-                textResultado.setText(media.toString());
+                if (media>=7)
+                    textResultado.setText("APROVADO!!! sua média é: " + media.toString()); // Saida
+                else {
+                    textResultado.setText("REPROVADO!!! sua média é: " + media.toString()); //Saida
+                }
             }
         });
     }
-
+    // Calculo
     private double calcularMedia(double n1, double n2) {
         double mediaFinal = (n1 + n2) / 2;
         return mediaFinal;
