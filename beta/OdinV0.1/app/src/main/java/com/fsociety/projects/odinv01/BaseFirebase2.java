bcf37f5d3fcc7a10c23e447f406c64b0fcde313c
Geilson Silva <geilsonpereira@outlook.com.br>
2016-12-12 23:10:22 -0300

    Add new BaseFirebase2

---
+++ beta/OdinV0.1/app/src/main/java/com/fsociety/projects/odinv01/BaseFirebase2.java
@@ -0,0 +1,13 @@
+package com.fsociety.projects.odinv01;
+
+import android.support.v7.app.AppCompatActivity;
+import android.os.Bundle;
+
+public class BaseFirebase2 extends AppCompatActivity {
+
+    @Override
+    protected void onCreate(Bundle savedInstanceState) {
+        super.onCreate(savedInstanceState);
+        setContentView(R.layout.activity_base_firebase2);
+    }
+}
